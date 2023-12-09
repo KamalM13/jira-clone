ba29c7cf79b581e8a8f9de3014a1e29d58d0137b
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2023-12-09 20:08:39 +0200

    Sidebar done

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,22 +3,23 @@ import { Plus } from "lucide-react"
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+import { MobileSideBar } from "./mobile-sidebar"
 
 export const Navbar = () => {
     return (
         <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b
         shadow-sm bg-white flex items-center">
-            {/*Mobile Navbar */}
+            <MobileSideBar />
             <div className="flex items-center gap-x-4">
                 <div className="hidden md:flex">
-                    <Logo/>
+                    <Logo />
                 </div>
                 <Button size="sm" className=" rounded-sm hidden md:block h-auto
                 py-1 px-2"variant="primary">
                     Create
                 </Button>
                 <Button size="sm" className="rounded-sm block md:hidden" variant="primary">
-                    <Plus className="h-4 w-4"/>
+                    <Plus className="h-4 w-4" />
                 </Button>
             </div>
             <div className="ml-auto flex items-center gap-x-2">
@@ -33,7 +34,7 @@ export const Navbar = () => {
                                 display: "flex",
                                 justifyContent: "center",
                                 alignItems: "center",
-                        }
+                            }
                         }
                     }} />
                 <UserButton
@@ -44,9 +45,10 @@ export const Navbar = () => {
                                 avatarBox: {
                                     height: "30",
                                     width: "30",
+                                }
+                            }
                         }
-                    }}
-                }/>
+                    } />
             </div>
         </nav>
     )
