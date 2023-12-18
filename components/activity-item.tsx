3dce283c1d651f3ddfd0d94fb0ffb7927a4c2771
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2023-12-18 18:29:25 +0200

    Final audit log fix

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -1,13 +1,34 @@
 import { AuditLog } from "@prisma/client"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { generateLogMessage } from "@/lib/generate-log-message"
-import { format } from "date-fns"
 
 interface ActivityItemProps {
     data: AuditLog
 }
 
+const formatDate = (inputDate:Date) => {
+    const date = new Date(inputDate);
+
+    // Extracting date components
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const day = date.getDate().toString().padStart(2, '0');
+    let hours = date.getHours()
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+
+    // Convert hours to 12-hour format
+    hours = (hours % 12) || 12;
+
+    // Creating the formatted date string
+    const formattedDate = `${year}-${month}-${day} at ${hours}:${minutes}:${seconds} ${meridiem}`;
+
+    return formattedDate;
+}
+
 export const ActivityItem = ({ data }: ActivityItemProps) => {
+    const date = formatDate(data.createdAt);
     return (
         <li className="flex items-center gap-x-2">
             <Avatar className="h-8 w-8">
@@ -20,7 +41,7 @@ export const ActivityItem = ({ data }: ActivityItemProps) => {
                     </span>{generateLogMessage(data)}
                 </p>
                 <p className="text-xs text-muted-foreground">
-                    {format(new Date(data.createdAt), "MMM d, yyyy 'at' h:mm a")}
+                    {date}
                 </p>
             </div>
         </li>
